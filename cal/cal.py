3c7eae50157507dd927438906debeebd631e707b
zhangwanli-marker <18321228089@163.com>
2020-12-10 14:31:06 +0800

    pytest

---
+++ cal/cal.py
@@ -0,0 +1,17 @@
+# -*-coding:utf-8-*-
+class Cal:
+    # 加法
+    def addition(self, x, y):
+        return x + y
+
+    # 减法
+    def substruction(self, x, y):
+        return x - y
+
+    # 乘法
+    def multiplication(self, x, y):
+        return x * y
+
+    # 除法
+    def division(self, x, y):
+        return x / y
