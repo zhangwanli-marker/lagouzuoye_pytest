da13676d68c46c49621269242877af3963adbbf4
zhangwanli-marker <18321228089@163.com>
2020-12-11 15:28:15 +0800

    pytest422222123232

---
+++ pytest_homework1/test_mycalculator/test_mycal.py
@@ -0,0 +1,20 @@
+# -*-coding:utf-8-*-
+class TestMyCal:
+    def setup(self):
+        pass
+
+    # 测试加法
+    def test_addition(self):
+        pass
+
+    # 测试减法
+    def test_substruction(self):
+        pass
+
+    # 测试乘法
+    def test_multiplication(self):
+        pass
+
+    # 测试除法
+    def test_division(self):
+        pass
